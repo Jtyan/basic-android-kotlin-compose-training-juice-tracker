fea3028688f5182c9b1abcefcbe58f85886560a4
Joeltt <joelttm15@gmail.com>
2025-05-07 17:17:38 +0100

    build: add play service ads dependency and meta-data

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,8 @@ android {
 dependencies {
 
     implementation(platform("androidx.compose:compose-bom:2024.12.01"))
+    // play services ads
+    implementation("com.google.android.gms:play-services-ads:22.2.0")
 
     implementation("androidx.activity:activity-compose:1.9.3")
     implementation("androidx.compose.material3:material3")
